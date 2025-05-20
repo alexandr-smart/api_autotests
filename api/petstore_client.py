e66c4a0bcecb8e11e85fb03859682b3e3cbb5fd5
ivanov_a <a.ivanov@limehd.tv>
2025-05-20 15:26:38 +0300

    petstore_client.py fix

---
+++ api/petstore_client.py
@@ -0,0 +1,15 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class PetstoreClient:
+    def __init__(self):
+        self.base_url = os.getenv("BASE_URL")
+
+    def get_pet_by_id(self, pet_id):
+        return requests.get(f"{self.base_url}/pet/{pet_id}")
+
+    def find_pets_by_status(self, status="available"):
+        return requests.get(f"{self.base_url}/pet/findByStatus", params={"status": status})
